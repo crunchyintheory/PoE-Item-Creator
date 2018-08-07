867f65674aa91d4352c0b76880a7cd48bf14162c
Sumner Rittby <sumner@crunchyintheory.com>
2018-08-06 19:27:42 -0500

    Enable hash urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true }
+      { enableTracing: true, useHash: true }
     ),
     HttpClientModule
   ],
