06c0cbcc22983777a7248c5de300fae4c9def5ef
Sigrid Rittby <sigrid@crunchyintheory.com>
2022-09-09 18:51:14 -0500

    Add elemental damage colors

--- src/app/value.pipe.ts
+++ src/app/value.pipe.ts
@@ -7,12 +7,22 @@ import { PropertyType } from './property';
 })
 export class ValuePipe implements PipeTransform {
 
+  private replacements: [RegExp, string][] = [
+    [/\<g\>(.*)\<\/g\>/g, '<span class="prop-gray">$1</span>'],
+    [/\<chaos\>(.*)\<\/chaos\>/g, '<span class="prop-chaos">$1</span>'],
+    [/\<fire\>(.*)\<\/fire\>/g, '<span class="prop-fire">$1</span>'],
+    [/\<cold\>(.*)\<\/cold\>/g, '<span class="prop-cold">$1</span>'],
+    [/\<lightning\>(.*)\<\/lightning\>/g, '<span class="prop-lightning">$1</span>']
+  ];
+
   transform(value: any, args?: any): any {
     if(value.type === PropertyType.StatReq) {
       return value.value.replace(/(\d+)/g, '<span class="prop-white">$1</span>');
     }
     else {
-      return value.value;
+      return this.replacements.reduce((acc, [regex, replacement]) => {
+        return acc.replaceAll(regex, replacement);
+      }, value.value as string);
     }
   }
 
