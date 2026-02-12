3aff75387735ac4813f27e266d65d477169f8e27
Sigrid Rittby <sigrid@crunchyintheory.com>
2026-02-12 13:04:05 -0500

    Allow rarity colors for custom tags

--- src/app/value.pipe.ts
+++ src/app/value.pipe.ts
@@ -11,14 +11,15 @@ export class ValuePipe implements PipeTransform {
   public constructor(public sanitizer: DomSanitizer) {}
 
   private replacements: [RegExp, string][] = [
-    [/%%g%%(.*)%%g%%/g, '<span class="prop-gray">$1</span>'],
-    [/%%w%%(.*)%%w%%/g, '<span class="prop-white">$1</span>'],
-    [/%%b%%(.*)%%b%%/g, '<span class="prop-blue">$1</span>'],
-    [/%%(?<tag>chaos|fire|cold|lightning|crafted|crucible|fractured|foulborn)%%(.*)%%\k<tag>%%/g, '<span class="prop-$1">$2</span>'],
-    [/%%(?<tag>i|em|strong)%%(.*)%%\k<tag>%%/g, '<$1>$2</$1>'],
-    [/%%u%%(.*)%%u%%/g, '<span style="text-decoration: underline">$1</span>'],
-    [/%%bold%%(.*)%%bold%%/g, '<strong>$1</strong>'],
-    [/%%color(#[a-fA-F0-9]{3,8})%%(.*)%%color(#[a-fA-F0-9]{3,8})?%%/g, '<span class="prop-color" style="color: $1">$2</span>']
+    [/%%g%%(.*)%%g%%/gi, '<span class="prop-gray">$1</span>'],
+    [/%%w%%(.*)%%w%%/gi, '<span class="prop-white">$1</span>'],
+    [/%%b%%(.*)%%b%%/gi, '<span class="prop-blue">$1</span>'],
+    [/%%(?<tag>chaos|fire|cold|lightning|crafted|crucible|fractured|foulborn)%%(.*)%%\k<tag>%%/gi, '<span class="prop-$1">$2</span>'],
+    [/%%(?<tag>normal|magic|rare|unique|currency|gem|prophecy|passive)%%(.*)%%\k<tag>%%/gi, '<span class="prop-$1">$2</span>'],
+    [/%%(?<tag>i|em|strong)%%(.*)%%\k<tag>%%/gi, '<$1>$2</$1>'],
+    [/%%u%%(.*)%%u%%/gi, '<span style="text-decoration: underline">$1</span>'],
+    [/%%bold%%(.*)%%bold%%/gi, '<strong>$1</strong>'],
+    [/%%color(#[a-fA-F0-9]{3,8})%%(.*)%%color(#[a-fA-F0-9]{3,8})?%%/gi, '<span class="prop-color" style="color: $1">$2</span>']
   ];
 
   transform(value: any, args?: any): any {
